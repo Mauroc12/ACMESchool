8346758d1fb543250d5ad63a24be3999bfec5f47
mauro castro <mauro.castro@sandinas.com.ar>
2024-08-01 16:00:37 -0300

    Estructura de la solucion

---
+++ ACMESchool.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ACMESchool.Domain
+{
+    public class Class1
+    {
+    }
+}
---
+++ ACMESchool.Persistence/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ACMESchool.Persistence
+{
+    public class Class1
+    {
+    }
+}
